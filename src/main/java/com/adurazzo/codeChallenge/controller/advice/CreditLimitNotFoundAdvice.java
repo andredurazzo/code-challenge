bd457b90e31a67a4b8e94c8a2a76c243f142a97c
andre.durazzo <andre.durazzo@b2wdigital.com>
2019-01-17 19:01:29 -0200

    feat: add exception advice

---
+++ src/main/java/com/adurazzo/codeChallenge/controller/advice/CreditLimitNotFoundAdvice.java
@@ -0,0 +1,19 @@
+package com.adurazzo.codeChallenge.controller.advice;
+
+import com.adurazzo.codeChallenge.exception.CreditLimitNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+class CreditLimitNotFoundAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(CreditLimitNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String employeeNotFoundHandler(CreditLimitNotFoundException ex) {
+        return ex.getMessage();
+    }
+}
